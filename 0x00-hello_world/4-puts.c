4448fe914fcb24a32eb038c92d22f57629a82fa6
ictauthority <njerukelvin53@gmail.com>
2023-07-14 01:52:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main -A c program that prints a line to the standard error
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
